18e70f16f0b29da34b728e021fb777b0addf46b7
Tim <tja2353@gmail.com>
2024-08-07 16:47:20 +0100

    Add vitest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AngularFireTestbed2024');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome to AngularFireTestbed2024');
   });
 });
